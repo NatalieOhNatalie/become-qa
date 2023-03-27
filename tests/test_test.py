df8e021232c44a277bfa4e276d004f50ac824b92
= <nataliia.maksymets@gliballogic.com>
2023-03-27 17:59:00 +0300

    env setup

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,2 +1,17 @@
-def test_test():
-    assert 1 == 1
+from src.config.config import config
+
+
+def test_user_age_is_43(user):
+    assert user.age == 42
+
+
+def test_user_age_is_50(user):
+    assert user.age == 42
+
+
+def test_http_request():
+    print(config.get("BASE_URL_API"))
+
+
+def test_ui_POM():
+    print(config.get("BASE_URL_UI"))
